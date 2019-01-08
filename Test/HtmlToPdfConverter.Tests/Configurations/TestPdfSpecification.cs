5264658fd41090861518a9b974454df0dfc8fee2
David Vanderheyden <david.vanderheyden@cronos.be>
2019-01-08 11:05:48 +0100

    Reference project in testing project

--- Test/HtmlToPdfConverter.Tests/Configurations/TestPdfSpecification.cs
+++ Test/HtmlToPdfConverter.Tests/Configurations/TestPdfSpecification.cs
@@ -1,17 +1,13 @@
-﻿using DinkToPdf;
-using HtmlToPdfConverter.Contracts.PageSettings;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HtmlToPdfConverter.Tests.Configurations
+﻿namespace HtmlToPdfConverter.Tests.Configurations
 {
-    public class TestPdfSpecification: BasePdfPageSpecification
+    using DinkToPdf;
+    using HtmlToPdfConverter.Contracts.PageSettingsAggregates;
+
+    public class TestPdfSpecification : BasePdfPageSpecification
     {
         public TestPdfSpecification()
-            :base(ColorMode.Color, new MarginSettings { Top = 20, Bottom = 20}, Orientation.Portrait, PaperKind.A4)
+            : base(ColorMode.Color, new MarginSettings { Top = 20, Bottom = 20 }, Orientation.Portrait, PaperKind.A4)
         {
-
         }
     }
 }
